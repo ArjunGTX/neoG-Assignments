38738f41a6ccb5dba38a6b89909988e39ede44e2
ArjunGTX <arjunatpavilion@gmail.com>
2022-01-12 21:13:39 +0530

    started week-1 assignments

---
+++ week-1/exersize.js
@@ -0,0 +1,47 @@
+//covert to es6
+
+// 1 ------------
+
+// var aloo = 1;
+// if (aloo == 1) {
+//    var a = 2;
+//    console.log(a);
+// }
+// console.log(aloo);
+
+// let aloo = 1;
+// if(aloo === 1) {
+//     let a = 2;
+//     console.log(a);
+// }
+// console.log(aloo);
+
+//  2 -----------------
+
+// var multiply = function(x, y) {
+//     return x * y;
+//   };
+
+// const multiply = (x, y) => x * y;
+
+//  3 ----------------
+
+// var customer = {
+//     name: "Bhaalo"
+//   };
+//   var card = {
+//     amount: 20,
+//     product: "Aaalo",
+//     unitprice: 50
+//   };
+//   var message = "Hello " + customer.name + " wants to buy " + card.amount + " " + card.product + " for price of " + card.unitprice + " per piece"
+
+// const customer = {
+//   name: "Bhaalo",
+// };
+// const card = {
+//     amount: 20,
+//     product: 'Aaalo',
+//     unitPrice: 50
+// };
+// let message = `Hello ${customer.name} wants to buy ${card.amount} ${card.product} for price of ${card.unitPrice} per piece`;
